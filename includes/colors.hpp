ae13980bfc237f6785168ee4f78eac1b087610b9
DocCodes <DocCodes@users.noreply.github.com>
2019-01-22 17:37:33 -0600

    :checkered_flag: Colors in newer Windows 10 Builds

--- includes/colors.hpp
+++ includes/colors.hpp
@@ -7,12 +7,9 @@
 #ifndef GPCU_COLORS_HPP_  // include guard
 #define GPCU_COLORS_HPP_
 
-namespace gpcu {
-  std::basic_string<char> getEnvVar(std::basic_string<char> key) {
-    char const* val = std::getenv(key.c_str());
-    return val == NULL ? std::basic_string<char>() : std::basic_string<char>(val);
-  }
+#include "os.hpp"
 
+namespace gpcu {
   namespace colors {
     enum Support {
       none = 1,
@@ -32,24 +29,31 @@ namespace gpcu {
       white
     };
 
-    Support getSupport() {
+    gpcu::colors::Support getSupport() {
+      gpcu::OperatingSystem os = gpcu::getOS();
       std::string term = gpcu::getEnvVar("TERM");
       std::string colorTerm = gpcu::getEnvVar("COLORTERM");
       std::string termProgram = gpcu::getEnvVar("TERM_PROGRAM");
 
-      if (!term.empty()) {
-        if (term == "dumb") {
-          return none;
+      if (term == "dumb") {
+        return none;
+      }
+      if (os == win32 || os == win64) {
+        std::string osVer = gpcu::getOSVer();
+        if (std::stof(osVer.substr(0, 2)) >= 10) {
+          if (std::stof(osVer.substr(5, osVer.length() - 5)) >= 10586) {
+            return std::stof(osVer.substr(5, osVer.length() - 5)) >= 14931 ? full : color;
+          }
         }
-        if (std::regex_match(term, std::regex("/-256(color)?$"))) {
-          return color;
-        }
-        if (std::regex_match(term, std::regex("/-16m(color)?$"))) {
-          return full;
-        }
-        if (std::regex_match(term, std::regex("/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/"))) {
-          return basic;
-        }
+      }
+      if (std::regex_match(term, std::regex("/-256(color)?$"))) {
+        return color;
+      }
+      if (std::regex_match(term, std::regex("/-16m(color)?$"))) {
+        return full;
+      }
+      if (std::regex_match(term, std::regex("/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/"))) {
+        return basic;
       }
       if (!colorTerm.empty()) {
         if (colorTerm == "truecolor") {
@@ -74,16 +78,20 @@ namespace gpcu {
 
     namespace wrap {
       std::string wrapper(std::string txt, int col, int off, int term) {
-        Support supportLevel = getSupport();
+        gpcu::colors::Support supportLevel = getSupport();
         bool isColor = true;
         if ((col < 10 && off == -30) && (term >= 22 && term <= 28)) {
           isColor = false;
         }
 
-        if ((supportLevel > Support::none) && (supportLevel == Support::basic || !isColor)) {
+        if (supportLevel == gpcu::colors::Support::none) {
+          return txt;
+        }
+
+        if (supportLevel == gpcu::colors::Support::basic || !isColor) {
           return wrapAnsi16(col, off) + txt + wrapAnsi16(term, -30);
         }
-        if (supportLevel >= Support::color && !isColor) {
+        if (supportLevel >= gpcu::colors::Support::color && !isColor) {
           return wrapAnsi256(col, off - 39) + txt + wrapAnsi16(term, -30);
         }
         return txt;
@@ -96,38 +104,38 @@ namespace gpcu {
       std::string inverse(std::string txt) { return wrapper(txt, 7, -30, 27); }
       std::string hidden(std::string txt) { return wrapper(txt, 8, -30, 28); }
       std::string strikethrough(std::string txt) { return wrapper(txt, 9, -30, 29); }
-      std::string black(std::string txt) { return wrapper(txt, Ansi::black, 0, 39); }
-      std::string red(std::string txt) { return wrapper(txt, Ansi::red, 0, 39); }
-      std::string green(std::string txt) { return wrapper(txt, Ansi::green, 0, 39); }
-      std::string yellow(std::string txt) { return wrapper(txt, Ansi::yellow, 0, 39); }
-      std::string blue(std::string txt) { return wrapper(txt, Ansi::blue, 0, 39); }
-      std::string magenta(std::string txt) { return wrapper(txt, Ansi::magenta, 0, 39); }
-      std::string cyan(std::string txt) { return wrapper(txt, Ansi::cyan, 0, 39); }
-      std::string white(std::string txt) { return wrapper(txt, Ansi::white, 0, 39); }
-      std::string bgBlack(std::string txt) { return wrapper(txt, Ansi::black, 10, 49); }
-      std::string bgRed(std::string txt) { return wrapper(txt, Ansi::red, 10, 49); }
-      std::string bgGreen(std::string txt) { return wrapper(txt, Ansi::green, 10, 49); }
-      std::string bgYellow(std::string txt) { return wrapper(txt, Ansi::yellow, 10, 49); }
-      std::string bgBlue(std::string txt) { return wrapper(txt, Ansi::blue, 10, 49); }
-      std::string bgMagenta(std::string txt) { return wrapper(txt, Ansi::magenta, 10, 49); }
-      std::string bgCyan(std::string txt) { return wrapper(txt, Ansi::cyan, 10, 49); }
-      std::string bgWhite(std::string txt) { return wrapper(txt, Ansi::white, 10, 49); }
-      std::string blackBright(std::string txt) { return wrapper(txt, Ansi::black, 60, 39); }
-      std::string redBright(std::string txt) { return wrapper(txt, Ansi::red, 60, 39); }
-      std::string greenBright(std::string txt) { return wrapper(txt, Ansi::green, 60, 39); }
-      std::string yellowBright(std::string txt) { return wrapper(txt, Ansi::yellow, 60, 39); }
-      std::string blueBright(std::string txt) { return wrapper(txt, Ansi::blue, 60, 39); }
-      std::string magentaBright(std::string txt) { return wrapper(txt, Ansi::magenta, 60, 39); }
-      std::string cyanBright(std::string txt) { return wrapper(txt, Ansi::cyan, 60, 39); }
-      std::string whiteBright(std::string txt) { return wrapper(txt, Ansi::white, 60, 39); }
-      std::string bgBlackBright(std::string txt) { return wrapper(txt, Ansi::black, 70, 49); }
-      std::string bgRedBright(std::string txt) { return wrapper(txt, Ansi::red, 70, 49); }
-      std::string bgGreenBright(std::string txt) { return wrapper(txt, Ansi::green, 70, 49); }
-      std::string bgYellowBright(std::string txt) { return wrapper(txt, Ansi::yellow, 70, 49); }
-      std::string bgBlueBright(std::string txt) { return wrapper(txt, Ansi::blue, 70, 49); }
-      std::string bgMagentaBright(std::string txt) { return wrapper(txt, Ansi::magenta, 70, 49); }
-      std::string bgCyanBright(std::string txt) { return wrapper(txt, Ansi::cyan, 70, 49); }
-      std::string bgWhiteBright(std::string txt) { return wrapper(txt, Ansi::white, 70, 49); }
+      std::string black(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::black, 0, 39); }
+      std::string red(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::red, 0, 39); }
+      std::string green(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::green, 0, 39); }
+      std::string yellow(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::yellow, 0, 39); }
+      std::string blue(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::blue, 0, 39); }
+      std::string magenta(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::magenta, 0, 39); }
+      std::string cyan(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::cyan, 0, 39); }
+      std::string white(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::white, 0, 39); }
+      std::string bgBlack(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::black, 10, 49); }
+      std::string bgRed(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::red, 10, 49); }
+      std::string bgGreen(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::green, 10, 49); }
+      std::string bgYellow(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::yellow, 10, 49); }
+      std::string bgBlue(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::blue, 10, 49); }
+      std::string bgMagenta(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::magenta, 10, 49); }
+      std::string bgCyan(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::cyan, 10, 49); }
+      std::string bgWhite(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::white, 10, 49); }
+      std::string blackBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::black, 60, 39); }
+      std::string redBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::red, 60, 39); }
+      std::string greenBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::green, 60, 39); }
+      std::string yellowBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::yellow, 60, 39); }
+      std::string blueBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::blue, 60, 39); }
+      std::string magentaBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::magenta, 60, 39); }
+      std::string cyanBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::cyan, 60, 39); }
+      std::string whiteBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::white, 60, 39); }
+      std::string bgBlackBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::black, 70, 49); }
+      std::string bgRedBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::red, 70, 49); }
+      std::string bgGreenBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::green, 70, 49); }
+      std::string bgYellowBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::yellow, 70, 49); }
+      std::string bgBlueBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::blue, 70, 49); }
+      std::string bgMagentaBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::magenta, 70, 49); }
+      std::string bgCyanBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::cyan, 70, 49); }
+      std::string bgWhiteBright(std::string txt) { return wrapper(txt, gpcu::colors::Ansi::white, 70, 49); }
     }  // namespace wrap
   }  // namespace colors
 }  // namespace gpcu
--- includes/os.hpp
+++ includes/os.hpp
@@ -7,8 +7,12 @@
 #ifndef GPCU_OS_HPP_  // include guard
 #define GPCU_OS_HPP_
 
+#include "join.hpp"
+#include "string.hpp"
+
 namespace gpcu {
   enum OperatingSystem {
+    other,
     DOS,
     linux,
     macOS9,
@@ -20,20 +24,17 @@ namespace gpcu {
     win64
   };
 
-  OperatingSystem os() {
-    #if defined(MSDOS)
-    return DOS;
-    #elif defined(__MSDOS__)
-    return DOS;
-    #elif defined(_MSDOS)
-    return DOS;
-    #elif defined(__DOS__)
+  std::string getEnvVar(std::string key) {
+    char const* val = std::getenv(key.c_str());
+    return val == NULL ? std::string() : std::string(val);
+  }
+
+  gpcu::OperatingSystem getOS() {
+    #if defined(MSDOS) || defined(__MSDOS__) || defined(_MSDOS) || defined(__DOS__)
     return DOS;
     #elif defined(__linux__)
     return linux;
-    #elif defined(macintosh)
-    return macOS9;
-    #elif defined(Macintosh)
+    #elif defined(macintosh) || defined(Macintosh)
     return macOS9;
     #elif defined(__APPLE__)
     return macOSX;
@@ -43,19 +44,67 @@ namespace gpcu {
     #else
     return sunOS;
     #endif
-    #elif defined(_WIN16)
+    #elif defined(_WIN16) || defined(__TOS_WIN__)
     return win16;
-    #elif defined(_WIN32)
+    #elif defined(_WIN32) || defined(__WIN32__) || defined(__WINDOWS__)
     return win32;
     #elif defined(_WIN64)
     return win64;
-    #elif defined(__WIN32__)
-    return win32;
-    #elif defined(__TOS_WIN__)
-    return win16;
-    #elif defined(__WINDOWS__)
-    return win32;
+    #else
+    return other;
     #endif
+  }
+
+  std::string joinPath(std::string a[], size_t size) {
+    gpcu::OperatingSystem os = getOS();
+    std::string pathSep = "/";
+
+    if (os == DOS || os == win16 || os == win32 || os == win64) {
+      pathSep = "\\";
+    }
+
+    return gpcu::join(a, size, pathSep);
+  }
+
+  std::string getTempDir() {
+    gpcu::OperatingSystem os = getOS();
+    std::string tempDir = std::string();
+
+    if (os == other) { return tempDir; }
+    if (tempDir.empty()) { tempDir = gpcu::getEnvVar("TMP"); }
+    if (tempDir.empty()) { tempDir = gpcu::getEnvVar("TEMP"); }
+    if (tempDir.empty()) { tempDir = gpcu::getEnvVar("TMPDIR"); }
+    if (tempDir.empty()) { tempDir = gpcu::getEnvVar("TEMPDIR"); }
+    if (tempDir.empty()) {
+      if (!(os == other || os == DOS || os == win16 || os == win32 || os == win64)) {
+        tempDir = "/tmp";
+      }
+    }
+
+    return tempDir;
+  }
+
+  std::string getTempFile() {
+    std::string tempJoin[2] = { gpcu::getTempDir(), "gpcu.txt" };
+    return gpcu::joinPath(tempJoin, 2);
+  }
+
+  std::string getOSVer() {
+    gpcu::OperatingSystem os = getOS();
+    std::string verPath = getTempFile();
+    std::string ver = std::string();
+    std::ifstream verFile(verPath);
+    std::stringstream buffer;
+
+    if (os == DOS || os == win16 || os == win32 || os == win64) {
+      std::system(("ver > " + verPath).c_str());
+      buffer << verFile.rdbuf();
+      ver = buffer.str();
+      ver = std::regex_replace(ver, std::regex("\\]?[^\\]]+(\\[|$)"), "");
+      ver = ver.substr(8, ver.length() - 8);
+    }
+
+    return ver;
   }
 }  // namespace gpcu
 
