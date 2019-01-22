51f044c63dade29101b14a0355cec4f84532695a
DocCodes <DocCodes@users.noreply.github.com>
2019-01-22 17:36:27 -0600

    :sparkles: String trimming

--- includes/string.hpp
+++ includes/string.hpp
@@ -12,9 +12,26 @@ namespace gpcu {
     void upper(std::string* s) {
       std::transform((*s).begin(), (*s).end(), (*s).begin(), ::toupper);
     }
-    
+
     void lower(std::string* s) {
       std::transform((*s).begin(), (*s).end(), (*s).begin(), ::tolower);
+    }
+
+    std::string& ltrim(std::string& s, const std::string& chars = "\t\n\v\f\r ")
+    {
+      s.erase(0, s.find_first_not_of(chars));
+      return s;
+    }
+
+    std::string& rtrim(std::string& s, const std::string& chars = "\t\n\v\f\r ")
+    {
+      s.erase(s.find_last_not_of(chars) + 1);
+      return s;
+    }
+
+    std::string& trim(std::string& s, const std::string& chars = "\t\n\v\f\r ")
+    {
+      return ltrim(rtrim(s, chars), chars);
     }
   }  // namespace string
 }  // namespace gpcu
