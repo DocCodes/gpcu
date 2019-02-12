e56cc51058d755551791addbeed6beb427f404dc
DocCodes <DocCodes@users.noreply.github.com>
2019-02-12 15:32:02 -0600

    :ambulance: Build numbers

--- includes/all.hpp
+++ includes/all.hpp
@@ -4,7 +4,7 @@
 *
 *  @author    Evan Elias Young
 *  @date      2019-01-17
-*  @date      2019-02-11
+*  @date      2019-02-12
 *  @copyright Copyright 2019 Evan Elias Young. All rights reserved.
 */
 
@@ -19,7 +19,7 @@
 * the next four contain the patch number,
 * and the last eight contain the revision number.
 */
-#define GPCU_VERSION_ 0x0205020031
+#define GPCU_VERSION_ 0x0205020033
 
 #include "color.hpp"
 #include "colors.hpp"
