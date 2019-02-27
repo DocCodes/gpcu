8ac3b0cde952c483de87388a0c51eac12de97d4b
Evan Elias Young <evanyoung762@gmail.com>
2019-02-27 08:32:07 -0600

    :bug: Pause header in the source file

--- includes/all.h
+++ includes/all.h
@@ -4,7 +4,7 @@
 *
 *  @author    Evan Elias Young
 *  @date      2019-01-17
-*  @date      2019-02-26
+*  @date      2019-02-27
 *  @copyright Copyright 2019 Evan Elias Young. All rights reserved.
 */
 
@@ -20,7 +20,7 @@
 * the next four contain the patch number,
 * and the last eight contain the revision number.
 */
-#define GPCU_VERSION_ 0x0206000039
+#define GPCU_VERSION_ 0x020601003A
 
 #include "stdafx.h"
 
--- includes/pause.cpp
+++ includes/pause.cpp
@@ -4,12 +4,12 @@
 *
 *  @author    Evan Elias Young
 *  @date      2019-02-26
-*  @date      2019-02-26
+*  @date      2019-02-27
 *  @copyright Copyright 2019 Evan Elias Young. All rights reserved.
 */
 
 #include "stdafx.h"
-#include "string.h"
+#include "pause.h"
 
 namespace gpcu
 {
