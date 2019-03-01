91683c77c43501db5a60ab7bf9a8af7807ed473c
DocCodes <DocCodes@users.noreply.github.com>
2019-03-01 15:07:49 -0600

    :memo: Fixing documentation mistake

--- includes/color.cpp
+++ includes/color.cpp
@@ -1,10 +1,10 @@
 /**
-*  @file      color.h
+*  @file      color.cpp
 *  @brief     Implementation for the color class.
 *
 *  @author    Evan Elias Young
 *  @date      2019-02-26
-*  @date      2019-02-26
+*  @date      2019-03-01
 *  @copyright Copyright 2019 Evan Elias Young. All rights reserved.
 */
 
