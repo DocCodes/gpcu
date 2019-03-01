ce3a53806c05810a331d052eb0b7a2db75fd7643
DocCodes <DocCodes@users.noreply.github.com>
2019-03-01 17:28:23 -0600

    :bug: Header guards

--- includes/color.h
+++ includes/color.h
@@ -4,9 +4,12 @@
 *
 *  @author    Evan Elias Young
 *  @date      2019-02-26
-*  @date      2019-02-26
+*  @date      2019-03-01
 *  @copyright Copyright 2019 Evan Elias Young. All rights reserved.
 */
+
+#ifndef GPCU_COLOR_
+#define GPCU_COLOR_
 
 #include "stdafx.h"
 
@@ -14,3 +17,5 @@ namespace gpcu
 {
 class Color;
 } // namespace gpcu
+
+#endif // GPCU_COLOR_
